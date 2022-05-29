b602eab7d21bd15c2cb2f07561faba34cafabc97
MitchelSong <songminsu2363@gmail.com>
2022-05-29 06:18:03 +0000

    [Feat] Making timeline in backend
    - Complete making face detection timeline in backend
    - Simply changing ml/face_timeline.py if change in face detection model
    issue #19

--- serving/backend/app/api/face.py
+++ serving/backend/app/api/face.py
@@ -4,7 +4,7 @@ import os
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 from datetime import datetime
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 from PIL import Image
 import base64
 import io
@@ -26,6 +26,10 @@ class People(BaseModel):
     message: Optional[str]
     # created_at : datetime = Field(default_factory=datetime.now)
 
+class ItemValue(BaseModel):
+    id: str
+    person: str
+
 
 def from_img_to_bytes(img):
     """
@@ -44,26 +48,36 @@ def from_img_to_bytes(img):
 async def show_people(id: UUID):
     # 사진 넣어주기
     people_img = {}
+    people_img_name = {}
     result_path = os.path.join(FILE_DIR, str(id), 'result')
     dir_list = os.listdir(result_path)
     people_list = [dir for dir in dir_list if dir.startswith('person')]
     for person in people_list:
-        img_file = os.listdir(os.path.join(str(result_path), str(person)))[0]
+        # 현재는 첫 번째 이미지를 가져옴. 이후에 다른 이미지를 가져오는 알고리즘이 있다면 사용하기
+        img_file = os.listdir(os.path.join(str(result_path), str(person)))[0]   # first_image
         img_path = os.path.join(str(result_path), str(person), str(img_file))
         img = Image.open(img_path)
         people_img[person] = from_img_to_bytes(img)
+        people_img_name[person] = str(img_file)
 
-    return {"id": id, "people_img": people_img, "message": "(show-people) Welcome to SNOWMAN :')"}
+    return {"id": id, "people_img": people_img, "people_img_name" : people_img_name, "message": "(show-people) Welcome to SNOWMAN :')"}
 
 
 @router.post("/timeline-face", description="face recognition을 통해 인물의 timeline을 추출한다.")
-async def get_timeline_face(face: UploadFile = File(...), id: UUID = None):
-    video = os.path.join(FILE_DIR, str(id), 'original.mp4')
-    image = face.file
+async def get_timeline_face(info: dict):
+    
+
+    result_path = os.path.join(FILE_DIR, info['id'])
+    print(result_path)
+    image_file = os.listdir(os.path.join(result_path, 'result', info['face']))[0]
+
+    video = os.path.join(result_path, 'original.mp4')
+    image = os.path.join(result_path, 'result', info['face'], image_file)
+    # print(image)
 
     timeline = make_face_timeline(video, image)
     # FE에서 선택한 사람을 받아 face recognition 진행 예정
-    return timeline
+    return {"id" : info['id'], "timeline": timeline}
 
 
 # TODO: /show-people (face clustering 결과 보여주기)
