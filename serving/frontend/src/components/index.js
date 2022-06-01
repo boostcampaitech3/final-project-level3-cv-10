8a499dfb564e493ab900d6aae1ee5aa1772dee54
seungriyou <seungri0826@gmail.com>
2022-06-01 12:03:15 +0000

    [Feat] Modify select-video page and components
    - SelectVideo 페이지와 내부 component인 Video, DownloadPanel을
    구현하였습니다.
    - 영상 선택 및 다운로드 기능, 모달창을 통한 영상 재생, 흥미도순 정렬
    등의 기능을 구현하였습니다.
    issue #27

--- serving/frontend/src/components/index.js
+++ serving/frontend/src/components/index.js
@@ -2,5 +2,6 @@ import Message from "./Message";
 import NavBar from "./NavBar";
 import PeoplePanel from "./PeoplePanel";
 import DownloadPanel from "./DownloadPanel";
+import Video from "./Video";
 
-export { Message, NavBar, PeoplePanel, DownloadPanel, };
+export { Message, NavBar, PeoplePanel, DownloadPanel, Video, };
