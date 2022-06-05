305ba59e4973af7668dc32fc7d681618bc08c95b
seungriyou <seungri0826@gmail.com>
2022-06-05 06:34:15 +0000

    [Feat] Resolve a problem of temporary files being created in root dir
    - Moviepy로 최종 쇼츠 영상을 생성할 때, temp 오디오 파일이 해당 uuid
    내의 폴더가 아닌 루트 디렉토리에 생성되는 문제를 해결하였습니다.
    - async로 여러 요청이 동시에 처리될 때 temp 파일의 이름이 같은 경우
    문제가 될 수 있다고 판단하였습니다.
    issue #58

--- serving/backend/app/ml/final_shorts/make_shorts.py
+++ serving/backend/app/ml/final_shorts/make_shorts.py
@@ -20,6 +20,8 @@ def make_shorts(final_highlights, total_length, id, target_person):
     VIDEO_DIR = os.path.join(FILE_DIR, id, "original.mp4")
     print(VIDEO_DIR)
 
+    CURRENT_DIR = os.getcwd()
+
     target_person_shorts = []
     for idx, (start, end, interest, during) in enumerate(final_highlights):
         print("Making Clips...")
@@ -29,9 +31,12 @@ def make_shorts(final_highlights, total_length, id, target_person):
         HIGHLISHT_STORAGE_DIR = os.path.join(SHORTS_STORAGE_DIR, f"short_{target_person}_{idx}.mp4")
         blob = bucket.blob(HIGHLISHT_STORAGE_DIR)
 
-        clip = VideoFileClip(VIDEO_DIR).subclip(start,end).fx(vfx.fadein,1).fx(vfx.fadeout,1)
-
+        clip = VideoFileClip(VIDEO_DIR).subclip(start,end).fx(vfx.fadein,1).fx(vfx.fadeout,1)        
+        
+        os.chdir(SHORTS_DIR) # for saving [video-name]TEMP_MPY_wvf_snd.mp3 file in files/[uuid]/shorts
         clip.write_videofile(HIGHLIGHT_PATH)
+        os.chdir(CURRENT_DIR) # come back to original directory
+
         blob.upload_from_filename(HIGHLIGHT_PATH)
 
         target_person_shorts.append([target_person, HIGHLISHT_STORAGE_DIR, during, interest])
