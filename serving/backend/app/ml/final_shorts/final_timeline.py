052cd56fe7863b6677424e8db56994ff7fbf578c
Shimjoonkyo <s980221@naver.com>
2022-06-08 23:39:32 +0900

    [Feat] Change code for making timelines
    타임라인 생성 및 흥미도 계산 방식 변경
    issue #85

--- serving/backend/app/ml/final_shorts/final_timeline.py
+++ serving/backend/app/ml/final_shorts/final_timeline.py
@@ -2,11 +2,11 @@ def make_final_timeline(laughter_timeline,person_timeline,max_length=None):
     p_timeline = person_timeline.copy()
     final = []
     total_length = 0
-    for ind,[start,end,interest] in enumerate(laughter_timeline):
+    for ind,(start,end,laugh_len,laugh_db) in enumerate(laughter_timeline):
         shot_length = end-start
         length=0
         while p_timeline:
-            s, e = p_timeline.pop(0)
+            s,e = p_timeline.pop(0)
             if e<start:
                 continue
             if s>end:
@@ -17,30 +17,34 @@ def make_final_timeline(laughter_timeline,person_timeline,max_length=None):
             length += (e-s)
         if shot_length < 35:
             if length/shot_length > 0.35:
-                final.append((start,end,interest,shot_length))
+                person_interest = (length/shot_length - 0.3)/0.4
+                total_interest = laugh_len*3 + laugh_db*2 + person_interest*1
+                final.append((round(start-5,2),end,round(total_interest,2),round(length/shot_length,3)))
                 total_length += shot_length
         else:
             if length/shot_length > 0.30:
-                final.append((start,end,interest,shot_length))
+                person_interest = (length/shot_length - 0.3)/0.4
+                total_interest = laugh_len*3 + laugh_db*2 + person_interest*1
+                final.append((round(start-5,2),end,round(total_interest,2),round(length/shot_length,3)))
                 total_length += shot_length  
             
-    # max_length 넘어가는 경우 등장비율 높은 순서로 max_length 이내로 선택
+    # max_length 넘어가는 경우 흥미도 높은 순서로 max_length 이내로 선택
     if max_length and total_length > max_length:
-        sorted_timeline = sorted(final,key=lambda x:-x[3])
+        sorted_timeline = sorted(final,key=lambda x:-x[2])
         choose_index = [False for _ in range(len(sorted_timeline))]
         total_length = 0
-        for start,end,ratio,interest in sorted_timeline:
+        for start,end,interest,ratio in sorted_timeline:
             length = end - start
             if total_length + length > max_length:
                 break
             else:
                 total_length += length
-                choose_index[final.index((start,end,ratio,interest))] = True
+                choose_index[final.index((start,end,interest,ratio))] = True
                 
         new_final = []
-        for ind, (s,e,r,i) in enumerate(final):
+        for ind, (s,e,i,r) in enumerate(final):
             if choose_index[ind]:
-                new_final.append((s,e,r,i))
+                new_final.append((s,e,i,r))
         final = new_final
     
     # interest 순서로 정렬
